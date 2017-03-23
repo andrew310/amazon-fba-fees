c5a60e8dd04b061deb0ff58e1b76badee19dee01
flyball <mattbrown600@gmail.com>
2017-03-23 02:24:13 -0700

    getting Canada test dialed in. Still a slight mismatch

--- fba/canada.py
+++ fba/canada.py
@@ -11,26 +11,32 @@ class Canada(Common):
         self.year = year
 
     def is_standard(self, l, w, h, g):
-        """Dims are in cm, weight in grams """
+        """Dims are in cm, weight in kilograms """
+
+        FOURPLACES = Decimal(10) ** -4
 
         # make sure all are floats
-        # values = list(map(lambda x: float(x), [l, w, h]))
-        values = [
-            float(l),
-            float(w),
-            float(h)
-            ]
+        values = list(
+            map(lambda x: Decimal(float(x)).quantize(FOURPLACES), [l, w, h]))
 
-        # kg = float(g) / 1000
-        kg = g
+        kg = Decimal(g).quantize(FOURPLACES)
 
-        # values.sort(reverse=True)
+        if (ceil(values[0]) > 45):
+            # print("CHECKING Length: " + str(l))
+            return False
+        if (ceil(values[1]) > 35):
+            # print("CHECKING width: " + str(w))
+            return False
+        if (ceil(values[2]) > 20):
+            # print("CHECKING height: " + str(h))
+            return False
+        if (ceil(kg) > 9):
+            # print("CHECKING weight: " + str(kg))
+            return False
 
-        return (values[0] <= 45 and values[1] <= 35
-                and values[2] <= 20 and kg <= 9)
+        return True
 
     def pick_and_pack(self, standard, media):
-        print("STANDARD RESULT: " + str(standard))
         if standard:
             return Decimal('0.90') if media else Decimal('1.55')
         else:
@@ -39,14 +45,16 @@ class Canada(Common):
     def weight_handling(self, weight):
         """Leaving out envelopes for now """
 
-        if weight <= 500:
+        weight_g = weight * 1000
+
+        if weight_g <= 500:
             return Decimal('3.75')
 
         else:
             # Oversize, 3.75 for first 500g, plus 0.36 for each add'l 500g
             return (Decimal('3.75')
                     + (Decimal('0.37')
-                    * ceil((weight - 500)/500)))
+                    * ceil((weight_g - 500)/500)))
 
     def get_monthly_storage(self, month, l=None, w=None, h=None):
         """Calculated per cubic meter """
@@ -79,12 +87,19 @@ class Canada(Common):
         if weight is None:
             return False
 
-        size = not self.is_standard(length, width, height, weight)
+        size = self.is_standard(length, width, height, weight)
+        # print("STANDARD RESULT: " + str(size))
         media = self.is_media(category)
 
         pnp = self.pick_and_pack(size, media)
         weight_handling = self.weight_handling(weight)
 
-        fee = pnp + weight_handling
+        monthly_storage = self.get_monthly_storage(3, length, width, height)
+
+        print('pnp: ' + str(pnp))
+        print('wh: ' + str(weight_handling))
+        print('monthly_storage: ' + str(monthly_storage))
+
+        fee = pnp + weight_handling + monthly_storage
 
         return round(Decimal(fee), 2)
